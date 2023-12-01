03566257ff35354cecd9a3b2b998ebe73c34c856
Krzysztof Zielinski <kzielinski@dataconsulting.pl>
2023-12-01 18:21:55 +0100

    Avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 
 streamlit.title("My first streamlit app ! :) ")
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Oatmeal')
-streamlit.text('Kale, SPinach & Rocket Smoothie')
-streamlit.text('Hard-Boilde Free-Rage Egg')
+streamlit.text('🥣 Omega 3 & Oatmeal')
+streamlit.text('🥗 Kale, SPinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boilde Free-Rage Egg')
+streamlit.text('🥑🍞 Avocado Toast')
