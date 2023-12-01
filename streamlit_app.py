b8c8be3c383b10555d1fe247a6e0fe1989d7c7b7
Krzysztof Zielinski <kzielinski@dataconsulting.pl>
2023-12-01 18:19:08 +0100

    Update text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title("My first streamlit app ! :) ")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Oatmeal')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Hard-Boilde Free-Rage Egg')
