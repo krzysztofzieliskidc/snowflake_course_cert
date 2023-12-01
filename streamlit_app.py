d743351567233e32d012c8f3eaac24452d6c79b1
Krzysztof Zielinski <kzielinski@dataconsulting.pl>
2023-12-01 19:25:02 +0100

    .index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My first streamlit app ! :) ")
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu') 
 streamlit.text('🥣 Omega 3 & Oatmeal')
 streamlit.text('🥗 Kale, SPinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boilde Free-Rage Egg')
@@ -13,7 +13,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #create pickup
-streamlit.multiselect("Pick some fruilts:",list(my_fruit_list))
+streamlit.multiselect("Pick some fruilts:",list(my_fruit_list.index ))
 
 #display table
 streamlit.dataframe(my_fruit_list)
