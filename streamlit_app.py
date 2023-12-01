fbbd9fb16daed598b530571ea68842a6f1cb2519
Krzysztof Zielinski <kzielinski@dataconsulting.pl>
2023-12-01 18:23:28 +0100

    streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Oatmeal')
 streamlit.text('🥗 Kale, SPinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boilde Free-Rage Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
